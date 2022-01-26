babee430ff30c20dc14f7a3ee6ab920b18d27159
QZLin <qzlin01@163.com>
2022-01-26 18:37:16 +0800

    pre-release 1.10.0

--- test_regex.sh
+++ test_regex.sh
@@ -1,3 +1,6 @@
 [[ `git log -1` =~ commit\ +([^[:space:]]*) ]]
 commit=${BASH_REMATCH[1]}
 commit=`git log -1 --format='%H'`
+
+if [[ $title =~ "release" ]] || [[ $title =~ "pre-release" ]];then echo "::info Not_Release";echo exit=true >> $GITHUB_ENV;fi
+if [[ $title =~ pre-release ]] ;then echo pre=true >> $GITHUB_ENV;else echo pre=false >> $GITHUB_ENV ;fi
