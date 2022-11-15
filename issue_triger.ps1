bdb4dc8a8b9ad889c96da7ea20adc7b4f0cdc093
QZLin <qzlin01@163.com>
2022-11-15 21:24:32 +0800

    update

---
+++ issue_triger.ps1
@@ -0,0 +1,23 @@
+Write-Output (Get-Location)
+gh issue list -s open |
+ForEach-Object { 
+    $line = $_ -split "\s+"
+    Write-Output ('"' + ($line -join '","') + '"')
+    $index = $line[0]
+    $title = $line[2]
+    if ($title -match "^/download$") {
+        $issue = gh issue view $index
+        Write-Verbose $issue
+
+        $text_start = $issue.IndexOf("--")
+        $text = $issue[($text_start + 1)..$issue.Length]
+        Write-Output $text
+
+        if ("$text" -match "(https?|ftp|file)://[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]") {
+            Write-Verbose $Matches
+            $url = $Matches[0]
+            wget $url
+            gh issue close $index
+        }
+    }
+}
