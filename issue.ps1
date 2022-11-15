8d7d13ea4557567410c6a787a498a1d4b009bd2d
QZLin <qzlin01@163.com>
2022-11-15 21:33:27 +0800

    11/15/2022 21:33:27

--- issue.ps1
+++ issue.ps1
@@ -1,13 +1,12 @@
 Write-Output (Get-Location)
-gh issue list -s open | ForEach-Object `
-{ 
+gh issue list -s open | ForEach-Object { 
     $line = $_ -split "\s+"
     Write-Output ('"' + ($line -join '","') + '"')
     $index = $line[0]
     $title = $line[2]
     if ($title -match "^/download$") {
         $issue = gh issue view $index
-        Write-Verbose $issue
+        Write-Verbose ($issue -join "`n")
 
         $text_start = $issue.IndexOf("--")
         $text = $issue[($text_start + 1)..$issue.Length]
